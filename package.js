4bcd377f4f36407a8baed57b0c48d1ca26b222f0
권정빈 <datamaker@urbanbase.com>
2017-09-13 15:13:24 +0900

    0.0.4 update

--- package.js
+++ package.js
@@ -1,19 +1,16 @@
 Package.describe({
   name: "datamaker:accounts-naver",
-  version: '0.0.1',
+  version: '0.0.4',
   summary: 'Login service for Naver',
   git: 'https://github.com/datamaker/meteor-accounts-naver',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use('accounts-base', ['client', 'server']);
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('spectrum:naver@0.0.1', ['client', 'server']);
-
-  api.addFiles('naver_login_button.css', 'client');
+  api.use('accounts-base@1.2.14', ['client', 'server']);
+  api.imply('accounts-base@1.2.14', ['client', 'server']);
+  api.use('accounts-oauth@1.1.15', ['client', 'server']);
+  api.use('datamaker:naver-oauth@0.0.4', ['client', 'server']);
 
   api.addFiles('naver.js');
 });
